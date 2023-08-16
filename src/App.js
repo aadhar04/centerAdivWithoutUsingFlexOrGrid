29d7139d6f07df280a7f91b471e61d1a7d5b28e1
aadhar04 <70593940+aadhar04@users.noreply.github.com>
2023-08-17 01:19:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="outerBox">
+        <div className="innerBox"></div>
+      </div>
+    </div>
+  );
+}
